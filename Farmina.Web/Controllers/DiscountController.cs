e0f9fd8a81d1f41993f5f534d3d0b387aed07025
SELMAN EE <s.ekici@mindfacture.de>
2019-04-06 12:01:05 +0300

    Farmina:
    Discount CRUD

--- Farmina.Web/Controllers/DiscountController.cs
+++ Farmina.Web/Controllers/DiscountController.cs
@@ -1,4 +1,6 @@
-﻿using Farmina.Web.DAL.Repository;
+﻿using Farmina.Web.BLL.Dto;
+using Farmina.Web.DAL.Entity;
+using Farmina.Web.DAL.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +17,58 @@ namespace Farmina.Web.Controllers
 			_fR = farminaRepository;
 		}
 
-
 		// GET: Discount
 		public ActionResult Index()
 		{
-			return View();
+			return View(_fR.GetByDescending<Discount>(o => o.Id));
+		}
+		public ActionResult Create()
+		{
+			return View(new Discount());
+		}
+		[HttpPost]
+		public ActionResult Create(Discount discount)
+		{
+			_fR.Add(discount);
+			_fR.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+		public ActionResult Edit(int id)
+		{
+			var discount = _fR.GetWhere<Discount>(s => s.Id == id)?.FirstOrDefault() ?? new Discount();
+			if (discount.Id <= 0)
+				return RedirectToAction("Index");
+
+			return View("Create", discount);
+		}
+		[HttpPost]
+		public ActionResult Edit(Discount discount)
+		{
+			var discountEntity = _fR.GetWhere<Discount>(s => s.Id == discount.Id)?.FirstOrDefault() ?? new Discount();
+			if (discount.Id <= 0)
+				return RedirectToAction("Index");
+			//
+			discountEntity.Name = discount.Name?.Trim();
+			discountEntity.FirstPercent = discount.FirstPercent;
+			discountEntity.SecondPercent = discount.SecondPercent;
+			discountEntity.ThirdPercent = discount.ThirdPercent;
+			discountEntity.FourthPercent = discount.FourthPercent;
+			//
+			_fR.Update(discountEntity);
+			_fR.SaveChanges();
+			//
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public ActionResult Delete(int id)
+		{
+			var discountEntity = _fR.GetWhere<Discount>(s => s.Id == id)?.FirstOrDefault() ?? new Discount();
+			//
+			_fR.Remove(discountEntity);
+			_fR.SaveChanges();
+			return Json(new Response { Status = true, EntityId = id, Message = "Silindi", TotalCount = 1 });
 		}
 	}
 }
--- Farmina.Web/Controllers/SupplierController.cs
+++ Farmina.Web/Controllers/SupplierController.cs
@@ -1,4 +1,6 @@
-﻿using Farmina.Web.DAL.Repository;
+﻿using Farmina.Web.BLL.Dto;
+using Farmina.Web.DAL.Entity;
+using Farmina.Web.DAL.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +17,61 @@ namespace Farmina.Web.Controllers
 			_fR = farminaRepository;
 		}
 
-
 		// GET: Supplier
 		public ActionResult Index()
 		{
-			return View();
+			return View(_fR.GetWhere<Supplier>(s => s.Status && !s.IsDeleted).OrderByDescending(o => o.Id));
+		}
+		public ActionResult Create()
+		{
+			return View(new Supplier());
+		}
+		[HttpPost]
+		public ActionResult Create(Supplier supplier)
+		{
+			supplier.CreatedDate = DateTime.Now;
+			supplier.Status = true;
+			_fR.Add(supplier);
+			_fR.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+		public ActionResult Edit(int id)
+		{
+			var supplier = _fR.GetWhere<Supplier>(s => s.Id == id && s.Status && !s.IsDeleted)?.FirstOrDefault() ?? new Supplier();
+			if (supplier.Id <= 0)
+				return RedirectToAction("Index");
+
+			return View("Create", supplier);
+		}
+		[HttpPost]
+		public ActionResult Edit(Supplier supplier)
+		{
+			var supplierEntity = _fR.GetWhere<Supplier>(s => s.Id == supplier.Id && s.Status && !s.IsDeleted)?.FirstOrDefault() ?? new Supplier();
+			if (supplier.Id <= 0)
+				return RedirectToAction("Index");
+			//
+			supplierEntity.Name = supplier.Name?.Trim();
+			supplierEntity.Code = supplier.Code?.Trim();
+			supplierEntity.UpdatedDate = DateTime.Now;
+			//
+			_fR.Update(supplierEntity);
+			_fR.SaveChanges();
+			//
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public ActionResult Delete(int id)
+		{
+			var supplierEntity = _fR.GetWhere<Supplier>(s => s.Id == id && s.Status && !s.IsDeleted)?.FirstOrDefault() ?? new Supplier();
+			supplierEntity.Status = false;
+			supplierEntity.IsDeleted = true;
+			supplierEntity.DeletedDate = DateTime.Now;
+			//
+			_fR.Update(supplierEntity);
+			_fR.SaveChanges();
+			return Json(new Response { Status = true, EntityId = id, Message = "Silindi", TotalCount = 1 });
 		}
 	}
 }
