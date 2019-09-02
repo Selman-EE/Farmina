27993c248a484d2d94e1006195a8701c483e9294
SELMAN EE <s.ekici@mindfacture.de>
2019-09-02 22:31:16 +0300

    Kiwigo:
    bugfix

--- Farmina.Web/Controllers/OrderController.cs
+++ Farmina.Web/Controllers/OrderController.cs
@@ -9,36 +9,36 @@ using System.Web.Mvc;
 
 namespace Farmina.Web.Controllers
 {
-	public class OrderController : BaseController
-	{
-		private readonly IFarminaRepository _fR;
-		public OrderController(IFarminaRepository farminaRepository)
-		{
-			_fR = farminaRepository;
-		}
+    public class OrderController : BaseController
+    {
+        private readonly IFarminaRepository _fR;
+        public OrderController(IFarminaRepository farminaRepository)
+        {
+            _fR = farminaRepository;
+        }
 
 
-		// GET: Order
-		public ActionResult Index()
-		{
-			return View(_fR.GetWhere<Order>(s => s.Status && !s.IsDeleted).OrderByDescending(o => o.Id));
-		}
-		public ActionResult Details(int id)
-		{
-			return View(_fR.GetWhere<Order>(x => x.Id == id).FirstOrDefault() ?? new Order());
-		}
-		[HttpPost]
-		public ActionResult Delete(int id)
-		{
-			var orderEntity = _fR.GetWhere<Order>(s => s.Id == id && s.Status && !s.IsDeleted)?.FirstOrDefault() ?? new Order();
-			orderEntity.Status = false;
-			orderEntity.IsDeleted = true;
-			orderEntity.DeletedDate = DateTime.Now;
-			//
-			_fR.Update(orderEntity);
-			_fR.SaveChanges();
-			return Json(new Response { Status = true, EntityId = id, Message = "Silindi", TotalCount = 1 });
-		}
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View(_fR.GetWhere<Order>(s => s.Status && !s.IsDeleted).OrderByDescending(o => o.Id));
+        }
+        public ActionResult Details(int id)
+        {
+            return View(_fR.GetWhere<Order>(x => x.Id == id && x.Status && !x.IsDeleted).FirstOrDefault() ?? new Order());
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var orderEntity = _fR.GetWhere<Order>(s => s.Id == id && s.Status && !s.IsDeleted)?.FirstOrDefault() ?? new Order();
+            orderEntity.Status = false;
+            orderEntity.IsDeleted = true;
+            orderEntity.DeletedDate = DateTime.Now;
+            //
+            _fR.Update(orderEntity);
+            _fR.SaveChanges();
+            return Json(new Response { Status = true, EntityId = id, Message = "Silindi", TotalCount = 1 });
+        }
 
-	}
+    }
 }
