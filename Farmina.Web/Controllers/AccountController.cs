495651eb40f9fcc61cf207dbe1752aef2d9c42cf
SELMAN EE <s.ekici@mindfacture.de>
2019-09-12 11:46:03 +0300

    Farmina:
    Session timeout

--- Farmina.Web/Controllers/AccountController.cs
+++ Farmina.Web/Controllers/AccountController.cs
@@ -12,100 +12,97 @@ using System.Web.Security;
 
 namespace Farmina.Web.Controllers
 {
-	public class AccountController : BaseController
-	{
-		private readonly IFarminaRepository _fR;
-		public AccountController(IFarminaRepository farminaRepository)
-		{
-			_fR = farminaRepository;
-		}
+    public class AccountController : BaseController
+    {
+        private readonly IFarminaRepository _fR;
+        public AccountController(IFarminaRepository farminaRepository)
+        {
+            _fR = farminaRepository;
+        }
 
-		//================================
-		// Business Logic
-		//================================
+        //================================
+        // Business Logic
+        //================================
 
-		/*
+        /*
         *  Login 
         */
-		[AllowAnonymous]
-		public ActionResult Login()
-		{
-			return View();
-		}
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
 
-		/*
+        /*
         *  Login post 
         */
-		[HttpPost]
-		[AllowAnonymous]
-		public ActionResult Login(LoginModel model)
-		{
-			//sing out all froms auth
-			FormsAuthentication.SignOut();
-			//
-			//check out if values null
-			if (model.Username.IsNull() || model.Password.IsNull())
-			{
-				//return error message
-				ModelState.AddModelError("ErrorMessage", "Kullanıcı adı veya parola boş bırakılamaz Lütfen doldurunuz");
-				return View("Login", model);
-			}
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model)
+        {
+            //sing out all froms auth
+            FormsAuthentication.SignOut();
+            //
+            //check out if values null
+            if (model.Username.IsNull() || model.Password.IsNull())
+            {
+                //return error message
+                ModelState.AddModelError("ErrorMessage", "Kullanıcı adı veya parola boş bırakılamaz Lütfen doldurunuz");
+                return View("Login", model);
+            }
 
-			if (model.Username?.Trim().ToLower() != "Username".GetAppSetting().ToLower() || model.Password?.Trim() != "Password".GetAppSetting())
-			{
-				//return error message
-				ModelState.AddModelError("ErrorMessage", "Kullanıcı adı veya parola yanlış. Lütfen kontol edip tekrar deneyin");
-				return View("Login", model);
-			}
-			//
-			//set session timeout
-			System.Web.HttpContext.Current.Session.Timeout = 1440;
-			//	
-			//save login data on cookie
-			CookieHelper.SetCookiesValue(model);
-			//
-			_fR.DeleteLoginLogMoreThanThreeMonths();
-			//
-			string hostName, userHostAddress;
-			try
-			{
-				userHostAddress = Helper.RequestHelpers.GetClientIpAddress(Request);
-				//
-				//get ip address
+            if (model.Username?.Trim().ToLower() != "Username".GetAppSetting().ToLower() || model.Password?.Trim() != "Password".GetAppSetting())
+            {
+                //return error message
+                ModelState.AddModelError("ErrorMessage", "Kullanıcı adı veya parola yanlış. Lütfen kontol edip tekrar deneyin");
+                return View("Login", model);
+            }
+            //	
+            //save login data on cookie
+            CookieHelper.SetCookiesValue(model);
+            //
+            _fR.DeleteLoginLogMoreThanThreeMonths();
+            //
+            string hostName, userHostAddress;
+            try
+            {
+                userHostAddress = Helper.RequestHelpers.GetClientIpAddress(Request);
+                //
+                //get ip address
 #pragma warning disable CS0618 // Type or member is obsolete
-				hostName = System.Net.Dns.GetHostByName(hostName: Environment.MachineName)?.AddressList[0]?.ToString() ?? "";
+                hostName = System.Net.Dns.GetHostByName(hostName: Environment.MachineName)?.AddressList[0]?.ToString() ?? "";
 #pragma warning restore CS0618 // Type or member is obsolete
 
-			}
-			catch (Exception ex)
-			{
-				hostName = ex?.InnerException?.Message ?? ex.Message;
-				userHostAddress = "";
-			}
-			//
-			//
-			_fR.Add(new AccountLog
-			{
-				HostName = hostName,
-				UserHostAddress = userHostAddress,
-				LogonUserIdentity = Request?.LogonUserIdentity?.Name ?? "",
-				LoggedTime = DateTime.Now,
-			});
-			_fR.SaveChanges();
-			//return main page
-			return RedirectToAction("Index", "Home");
-		}
+            }
+            catch (Exception ex)
+            {
+                hostName = ex?.InnerException?.Message ?? ex.Message;
+                userHostAddress = "";
+            }
+            //
+            //
+            _fR.Add(new AccountLog
+            {
+                HostName = $"{Environment.MachineName} - {hostName}",
+                UserHostAddress = userHostAddress,
+                LogonUserIdentity = Request?.LogonUserIdentity?.Name ?? "",
+                LoggedTime = DateTime.Now,
+            });
+            _fR.SaveChanges();
+            //return main page
+            return RedirectToAction("Index", "Home");
+        }
 
-		/*
+        /*
         *  Login out
         */
-		public ActionResult Logout()
-		{
-			//clear cookie
-			CookieHelper.ClearCookies();
-			//sing out all froms auth
-			FormsAuthentication.SignOut();
-			return View("Login");
-		}
-	}
+        public ActionResult Logout()
+        {
+            //clear cookie
+            CookieHelper.ClearCookies();
+            //sing out all froms auth
+            FormsAuthentication.SignOut();
+            return View("Login");
+        }
+    }
 }
